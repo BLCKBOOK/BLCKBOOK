877011786e4e95cf49174ce9766f1987be81bfc2
Your Name <simonschiebler@gmail.com>
2021-09-08 18:20:25 +0200

    api schema for initArtworkUpload

--- backend/src/rest/artwork/initArtworkUpload/apiSchema.ts
+++ backend/src/rest/artwork/initArtworkUpload/apiSchema.ts
@@ -1,32 +1,32 @@
 export const initArtworkUploadSchema = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type": "object",
-    "properties": {
-      "title": {
-        "type": "string"
-      },
-      "contentType": {
-        "type": "string"
-      },
-      "longitude": {
-        "type": "string"
-      },
-      "latitude": {
-        "type": "string"
-      }
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "title": {
+      "type": "string"
     },
-    "required": [
-      "contentType",
-      "longitude",
-      "latitude"
-    ]
-  }
-  
-  export interface initArtworkUploadRequest {
-    title?: string,
-    contentType: string,
-    longitude: string,
-    latitude: string
-  }
+    "contentType": {
+      "type": "string"
+    },
+    "longitude": {
+      "type": "string"
+    },
+    "latitude": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "contentType",
+    "longitude",
+    "latitude"
+  ]
+}
 
-  export type initArtworkUploadResponse = String
+export interface initArtworkUploadRequest {
+  title?: string,
+  contentType: string,
+  longitude: string,
+  latitude: string
+}
+
+export type initArtworkUploadResponse = String
