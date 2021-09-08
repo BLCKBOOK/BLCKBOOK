cd388222de7a437b91d373f82012de2bf0a5ba8f
Your Name <simonschiebler@gmail.com>
2021-09-08 14:45:50 +0200

    zpdate Dynamodb after image upload to s3

--- backend/src/eventTriggered/updateUserstateAfterUpload.ts
+++ backend/src/eventTriggered/updateUserstateAfterUpload.ts
@@ -1,7 +1,9 @@
 import { DynamoDB,GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { S3Client,HeadBucketCommand,HeadObjectCommand } from "@aws-sdk/client-s3";
 import { userInfo } from "../artwork.interface";
 
-const client = new DynamoDB({ region: process.env['AWS_REGION'] });
+const DDBClient = new DynamoDB({ region: process.env['AWS_REGION'] });
+const s3Client = new S3Client({region: process.env['AWS_REGION']})
 
 module.exports.handler =  async (event, context) => {
   console.log("event");
@@ -13,10 +15,11 @@ module.exports.handler =  async (event, context) => {
 
   for (let index = 0; index < event.Records.length; index++) {
     const record = event.Records[index];
-    const username = record.s3.object.key.split('/')[1]
+    const newImage = record.s3.object
+    const username = newImage.key.split('/')[1]
 
     const getItemCommand = new GetItemCommand({ TableName: process.env['USER_INFO_TABLE_NAME'], Key: { username: { S: username } }, ConsistentRead:true});
-    const Item = await (await client.send(getItemCommand)).Item;
+    const Item = await (await DDBClient.send(getItemCommand)).Item;
     console.log(Item);
     Item.uploadsDuringThisPeriod.N = String(Number(Item.uploadsDuringThisPeriod.N)+1)
     console.log(Item);
@@ -27,9 +30,59 @@ module.exports.handler =  async (event, context) => {
     });
 
     console.log(updateItemCommand)
-    console.log(await client.send(updateItemCommand))
+    console.log(await DDBClient.send(updateItemCommand))
     
-    // const imageUrl= `https://${record.s3.bucket.name}.s3.${record.awsRegion}.amazonaws.com/${record.s3.object.key}`
-    // TODO update image data in dynamoDb
+    
+    
+    // get image metadata from s3
+    const getImageMetadata = new HeadObjectCommand({
+      Bucket:record.s3.bucket.name,
+      Key:newImage.key,
+    })
+    const metadata = await (await s3Client.send(getImageMetadata)).Metadata
+    console.log(metadata)
+
+    // post image metadata with s3 url to dynamodb
+    const imageUrl = `https://${record.s3.bucket.name}.s3.${record.awsRegion}.amazonaws.com/${record.s3.object.key}`
+    const createNewUserObjectCommand = new PutItemCommand({
+      TableName: process.env['UPLOADED_ARTWORKS_TABLE_NAME'],
+      Item: {
+        longitude: {
+          N: metadata.longitude
+        },
+        latitude: {
+          N: metadata.latitude
+        },
+        periodId:{
+          S: metadata.periodid
+        },
+        artworkId:{
+          S: metadata.artworkid
+        },
+        contentType:{
+          S: metadata.contenttype
+        },
+        uploadTimestamp :{
+          N: metadata.uploadtimestamp
+        },
+        approvalState:{
+          S: metadata.approvalstate
+        },
+        title:{
+          S: metadata.title
+        },
+        uploader:{
+          S: metadata.uploader
+        },
+        imageUrl:{
+          S: imageUrl
+        }
+      }
+    })
+    console.log(createNewUserObjectCommand)
+    const newItem = await DDBClient.send(createNewUserObjectCommand)
+    console.log(newItem)
+
+    return event
   }
 }
