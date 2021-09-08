e1d62580956a00c6bfe91d778cb22d79472305e1
Your Name <simonschiebler@gmail.com>
2021-09-08 15:08:35 +0200

    add schema validation to initImageUpload

--- backend/src/common/responses.ts
+++ backend/src/common/responses.ts
@@ -1,3 +1,2 @@
 
 export const maxUploadCountReached = {statusCode:503, body:"Maximum number of uploads reached."}
-export const userCreated(username:string) => {return {statusCode:200, body:`User ${username} was created`}}
