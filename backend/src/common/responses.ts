a1afef8f8e5ea4ed16a0d6401028eeed28ae0a5f
Your Name <simonschiebler@gmail.com>
2021-09-09 22:47:35 +0200

    added more responses

--- backend/src/common/responses.ts
+++ backend/src/common/responses.ts
@@ -1,2 +1,4 @@
 
-export const maxUploadCountReached = { statusCode: 503, body: "Maximum number of uploads reached." }
+export const maxUploadCountReached = { statusCode: 503, body: "Upload not allowed. Max uploads reached" }
+export const wrongRequestBodyFormat = { statusCode: 400, message: "The request body doesn't have the correct format." }
+export const wrongContentType = { statusCode: 400, message: "Content Type not supported. Only JPG, PNG and GIF are accepted" }
