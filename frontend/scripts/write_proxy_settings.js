974b232b63d47ae8c87753455f9987d2445a024d
NielsCo <89005445+NielsCo@users.noreply.github.com>
2021-09-09 13:19:11 +0200

    added a script that sets the proxy-config

---
+++ frontend/scripts/write_proxy_settings.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const aws_export = 'src/aws-exports.ts'
+const proxy_settings = 'src/proxy.conf.json'
+let endpoint;
+
+fs.readFile(aws_export, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err)
+  }
+  const endpointIndex = data.indexOf('endpoint: \'https:');
+  const firstIndex = data.indexOf('\'', endpointIndex + 1) + 1;
+  const secondIndex = data.indexOf('\'', firstIndex + 1);
+  endpoint = data.substr(firstIndex, secondIndex - firstIndex);
+  endpoint = endpoint.substr(0, endpoint.lastIndexOf('/'));
+})
+
+fs.readFile(proxy_settings, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err)
+  }
+
+  const json = JSON.parse(data);
+
+  json['/dev'].target = endpoint;
+  json['/api'].target = endpoint;
+
+  const result = JSON.stringify(json, null, 2);
+
+  fs.writeFile(proxy_settings, result, 'utf8', function (err) {
+    if (err) return console.log(err)
+  })
+})
