bda23ea42cb123ad6c7ddb4204242da01be5f6f7
Simon Schiebler <simonschiebler@gmail.com>
2021-09-23 18:31:23 +0200

    notifications

--- backend/src/rest/notifications/apiSchema.ts
+++ backend/src/rest/notifications/apiSchema.ts
@@ -1,5 +1,68 @@
 import { Notification, NotificationIndex } from "../../common/tableDefinitions";
 
-export type getNotificationsPageRequestBody = { index: NotificationIndex, limit: number }
+export const getNotificationsPageRequestSchema = {
+  "type": "object",
+  "properties": {
+    "queryStringParameters": {
+      "type": "object",
+      "properties": {
+        "limit": {
+          "type": "integer"
+        },
+        "lastKey": {
+          "type": "string",
+        }
+      },
+      "required": [
+      ]
+    }
+  },
+  "required": [
+    
+  ]
+}
 
-export type getNotificationsPageResponseBody = Notification[]
+export type getNotificationsRequestQueryParams = { lastKey?: NotificationIndex, limit?: number }
+export type getNotificationsResponseBody = {notifications: Notification[], lastKey: {
+  userId: {S: string},
+  timestamp: {S: string},
+}}
+
+export const seeNotifivationsRequestSchema={
+  "type": "object",
+  "properties": {
+    "body": {
+      "type": "object",
+      "properties": {
+        "notifications": {
+          "type": "array",
+          "items": {
+              "type": "object",
+              "properties": {
+                "userId": {
+                  "type": "string"
+                },
+                "timestamp": {
+                  "type": "string"
+                }
+              },
+              "required": [
+                "userId",
+                "timestamp"
+              ]
+            },
+          "minItems": 1
+        }
+      },
+      "required": [
+        "notifications"
+      ]
+    }
+  },
+  "required": [
+    "body"
+  ]
+}
+
+export type setSeenRequestBody = { notifications: NotificationIndex[]}
+export type setSeenResponseBody = string
--- backend/src/rest/notifications/get.ts
+++ backend/src/rest/notifications/get.ts
@@ -7,40 +7,54 @@ import { createError, getInternal } from "@middy/util";
 
 import { validate } from "jsonschema";
 
-import { getNotificationsPageRequestBody, getNotificationsPageResponseBody } from "./apiSchema";
+import { getNotificationsRequestQueryParams, getNotificationsResponseBody,getNotificationsPageRequestSchema } from "./apiSchema";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
-import { UserInfo, VotableArtwork } from "../../common/tableDefinitions";
+import { Notification } from "../../common/tableDefinitions";
 import { LambdaResponseToApiGw } from "../../common/lambdaResponseToApiGw";
 import AuthMiddleware from "../../common/AuthMiddleware"
 import RequestLogger from "../../common/RequestLogger";
+import jsonBodyParser from "@middy/http-json-body-parser";
+import validator from "@middy/validator";
 
 const DDBclient = new DynamoDBClient({ region: process.env['AWS_REGION'] });
-let returnObject: getNotificationsPageResponseBody;
+let returnObject: getNotificationsResponseBody;
 
 const baseHandler = async (event, context): Promise<LambdaResponseToApiGw> => {
     const userId = event.requestContext.authorizer.claims['sub']
-    const body = event.body as getNotificationsPageRequestBody;
+    let lastKey = event['queryStringParameters']['lastKey'] ? JSON.parse(event['queryStringParameters']['lastKey']) : undefined
+    let limit = event['queryStringParameters']['limit'] ? JSON.parse(event['queryStringParameters']['limit']) : undefined
+
+    if (!limit)
+     limit = 8;
+    if (limit > 50)
+     limit = 50
 
     const query = new QueryCommand({
         TableName: process.env['NOTIFICATION_TABLE_NAME'],
         KeyConditionExpression: "userId = :userId",
         ExpressionAttributeValues: marshall({ ":userId": userId }),
-        IndexName: "uploaderIndex",
         ScanIndexForward: false,
-        Limit: body.limit
+        Limit: limit,
     })
+    if(lastKey)
+        query.input.ExclusiveStartKey =  lastKey;
     const queryResponse = await DDBclient.send(query)
     const queriedNotifications = queryResponse.Items;
-    if (!queriedNotifications || queriedNotifications.length == 0)
-        return Promise.reject(createError(404, "No Notifications found"))
+    lastKey = queryResponse.LastEvaluatedKey
+    if (!queriedNotifications)
+        return Promise.reject(createError(500, "Internal server error"))
 
-    returnObject = (queriedNotifications.map(notification => unmarshall(notification))) as getNotificationsPageResponseBody
+    const notifications = (queriedNotifications.map(notification => unmarshall(notification))) as Notification[]
+    returnObject = {notifications,lastKey}
+    console.log(returnObject)
     return { statusCode: 200, headers: { "content-type": "application/json" }, body: JSON.stringify(returnObject) };
 }
 
 const handler = middy(baseHandler)
     .use(cors({ origin: process.env['FRONTEND_HOST_NAME'] }))
+    .use(jsonBodyParser())
     .use(RequestLogger())
+    .use(validator({inputSchema:getNotificationsPageRequestSchema}))
     .use(AuthMiddleware(['User', 'Admin']))
     .use(httpErrorHandler())
 
