c6c1ae3e9b99015c7b3c38d6bab099b8e53e5c93
NielsCo <89005445+NielsCo@users.noreply.github.com>
2022-08-24 13:46:24 +0200

    package updates

--- frontend/src/app/services/AuthInterceptor.ts
+++ frontend/src/app/services/AuthInterceptor.ts
@@ -1,9 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import parse from 'url-parse';
 import {environment} from '../../environments/environment';
 import {AuthenticatorService} from '@aws-amplify/ui-angular';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -13,8 +13,9 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const session = this.authenticator.user?.getSignInUserSession();
-    if (!session || parse(req.url).pathname.startsWith('/artwork') || !session.isValid()
+    if (!session || req.url.includes('amazonaws.com/artwork') || !session.isValid()
       || req.url.startsWith(environment.pinataGateway) || req.url.startsWith(environment.betterCallDevAddress)) {
+      console.log('we are here');
       return next.handle(req);
     }
     const token = session.getIdToken().getJwtToken();
