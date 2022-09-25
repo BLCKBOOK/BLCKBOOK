812c70eaba0e9c9e6a4e561f3b98e9ff12ae423f
Simon Schiebler <simonschiebler@gmail.com>
2022-09-25 08:42:25 +0200

    fix

--- backend/src/eventTriggered/minter/minter.ts
+++ backend/src/eventTriggered/minter/minter.ts
@@ -122,6 +122,8 @@ const baseHandler = async (event, context) => {
     
     const theVoteAddress = process.env['THE_VOTE_CONTRACT_ADDRESS']
     if (!theVoteAddress) throw new Error(`THE_VOTE_CONTRACT_ADDRESS env variable not set`)
+    
+    const awsAccountId = context.invokedFunctionArn.split(':')[4]
 
     const tezos = new TezosToolkit(rpc);
     const admin = await getTezosAdminAccount()
