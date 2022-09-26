19da205792e1fefcbd141ec7a78f37ed52cce124
NielsCo <niels@blckbook.xyz>
2022-09-26 12:40:16 +0200

    updates setUser to work with prod

--- backend/src/common/setUser.ts
+++ backend/src/common/setUser.ts
@@ -1,18 +1,8 @@
 import { importKey, InMemorySigner } from "@taquito/signer";
 import { TezosToolkit } from "@taquito/taquito";
-import { Tzip16Module } from "@taquito/tzip16";
 import { TaquitoUser } from "./taquitoUser";
 
 export async function setUser(tezos: TezosToolkit, currentUser: TaquitoUser) {
-    tezos.setSignerProvider(InMemorySigner.fromFundraiser(currentUser.email, currentUser.password, currentUser.mnemonic.join(' ')));
-
-    tezos.addExtension(new Tzip16Module());
-
-    importKey(
-        tezos,
-        currentUser.email,
-        currentUser.password,
-        currentUser.mnemonic.join(' '),
-        currentUser.activation_code
-    ).catch((e: any) => console.error(e));
+    tezos.setSignerProvider(await InMemorySigner.fromSecretKey(currentUser.privateKey));
+    await importKey(tezos, currentUser.privateKey);
 }
--- backend/src/common/taquitoUser.ts
+++ backend/src/common/taquitoUser.ts
@@ -1,7 +1,4 @@
 export interface TaquitoUser {
-    email: string,
-    password: string,
-    mnemonic: string[],
-    activation_code: string,
     pkh: string,
+    privateKey: string,
 }
